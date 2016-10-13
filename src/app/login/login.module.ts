d01e2d90d3b23f01a087f1641520e6aba33ec27e
aReeMJay <rmj@dma.dk>
2016-10-13 11:40:24 +0200

    Added proper frontpage/loginpage
    Also updated icons and styling

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import {NgaModule} from "../theme/nga.module";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgaModule
   ],
   declarations: [LoginComponent]
 })
