fb10d6a5588c06fdde4013044a8a529070bff2d7
aReeMJay <rmj@dma.dk>
2017-02-13 14:31:20 +0100

    removed console.log

--- src/app/theme/validators/checkbox.validator.ts
+++ src/app/theme/validators/checkbox.validator.ts
@@ -6,7 +6,6 @@ const NOT_VALID = {
 }
 export class CheckboxValidator {
   public static validate(c:AbstractControl) {
-  	console.log("VVVVVV: ",c);
 	  if (!c.value) {
 		  return NOT_VALID;
 	  } else {
