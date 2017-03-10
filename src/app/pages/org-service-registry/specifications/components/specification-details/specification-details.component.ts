09d2136bf338d75871035b23f2fd4d3afb8b27b2
aReeMJay <rmj@dma.dk>
2017-03-10 08:18:54 +0100

    Some endorsement finetuning

--- src/app/pages/org-service-registry/specifications/components/specification-details/specification-details.component.ts
+++ src/app/pages/org-service-registry/specifications/components/specification-details/specification-details.component.ts
@@ -143,7 +143,7 @@ export class SpecificationDetailsComponent {
   	// TODO Maybe make an EndorseButton, as this will be used 3 places
 	  this.endorseButtonTitle = (this.isEndorsedByMyOrg ? 'Dedorse Specification' : 'Endorse Specification');
 	  this.endorseButtonClass = (this.isEndorsedByMyOrg ? 'btn btn-danger btn-raised' : 'btn btn-success btn-raised btn-with-icon');
-	  this.endorseButtonIcon = (this.isEndorsedByMyOrg ? '' : 'ion-android-cloud-done');
+	  this.endorseButtonIcon = (this.isEndorsedByMyOrg ? '' : 'ion-android-cloud-done fa-lg');
   }
 
 	private loadOrganizationName() {
