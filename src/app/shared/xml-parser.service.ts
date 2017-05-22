9dfce673b9447d6215a85c0cfd0fbada441aed32
aReeMJay <rmj@dma.dk>
2017-05-22 14:35:25 +0200

    Firefox not as clever as chrome in xml parsing.

--- src/app/shared/xml-parser.service.ts
+++ src/app/shared/xml-parser.service.ts
@@ -13,8 +13,19 @@ export class XmlParserService {
       let xmlString =  xml.content;
       var xmlData = parser.parseFromString(xmlString, xml.contentContentType);
 
-      let innerXmlData = xmlData.getElementsByTagName(outerField)[0];
-      return innerXmlData.getElementsByTagName(fieldToFind)[0].childNodes[0].nodeValue;
+	    let outerElement = xmlData.getElementsByTagName(outerField);
+	    if (outerElement.length == 0) {
+	    	let prefix = xmlData.documentElement.prefix;
+	    	outerElement = xmlData.getElementsByTagName(prefix + ":" + outerField);
+	    }
+
+	    let innerXmlData = outerElement[0];
+	    let innerElement = innerXmlData.getElementsByTagName(fieldToFind);
+	    if (innerElement.length == 0) {
+		    let prefix = xmlData.documentElement.prefix;
+		    innerElement = innerXmlData.getElementsByTagName(prefix + ":" + fieldToFind);
+	    }
+      return innerElement[0].childNodes[0].nodeValue;
     } catch ( error ) {
       throw new Error("Error trying to parse required field: " + outerField + " -> " + fieldToFind);
     }
@@ -26,7 +37,12 @@ export class XmlParserService {
       let xmlString =  xml.content;
       var xmlData = parser.parseFromString(xmlString, xml.contentContentType);
 
-      return xmlData.getElementsByTagName(field)[0].childNodes[0].nodeValue;
+	    let innerElement = xmlData.getElementsByTagName(field);
+	    if (innerElement.length == 0) {
+		    let prefix = xmlData.documentElement.prefix;
+		    innerElement = xmlData.getElementsByTagName(prefix + ":" + field);
+	    }
+	    return innerElement[0].childNodes[0].nodeValue;
     } catch ( error ) {
       throw new Error("Error trying to parse required field: " + field);
     }
