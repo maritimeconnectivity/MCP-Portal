ac9959ccbcae21a98515b258910f2f72d96028b0
Oliver Haagh <oliver@dmc.international>
2020-06-26 15:03:33 +0200

    Remove STM enrollment checkbox

--- src/app/apply-org/apply-org.component.ts
+++ src/app/apply-org/apply-org.component.ts
@@ -129,12 +129,6 @@ export class ApplyOrgComponent implements OnInit {
 		this.registerForm.addControl(formControlModel.elementId, formControl);
 		this.formControlModels.push(formControlModel);
 
-		let formControlModelCheck:McFormControlModelCheckbox = {state: false, formGroup: this.registerForm, elementId: 'isStm', controlType: McFormControlType.Checkbox, labelName: 'Enroll the organization via STM'};
-		formControl = new FormControl({value: formControlModelCheck.state, disabled: false}, formControlModelCheck.validator);
-		formControl.valueChanges.subscribe(param => this.setMrnMask(param));
-		this.registerForm.addControl(formControlModelCheck.elementId, formControl);
-		this.formControlModels.push(formControlModelCheck);
-
 		formControlModel = {formGroup: this.registerForm, elementId: 'orgId', controlType: McFormControlType.Text, labelName: 'Organization shortname', placeholder: 'Enter shortname to generate MRN', validator:Validators.required, pattern:this.mrnPattern, errorText:this.mrnPatternError};
 		formControl = new FormControl('', formControlModel.validator);
 		formControl.valueChanges.subscribe(param => this.generateMRN(param));
