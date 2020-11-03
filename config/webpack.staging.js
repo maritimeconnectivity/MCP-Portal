2a5fb41d7e2df4b3bf97209b363dc62d0644221f
Oliver Haagh <oliver@dmc.international>
2020-11-03 16:35:46 +0100

    Fix config for staging

--- config/webpack.staging.js
+++ config/webpack.staging.js
@@ -115,7 +115,7 @@ module.exports = function(options) {
           'KEYCLOAK_JSON':JSON.stringify('assets/staging-keycloak.json'),
           'ENVIRONMENT_TEXT':JSON.stringify(METADATA.BANNER_TEXT),
           'IS_DEV': false,
-          'CAN_JOIN': false,
+          'CAN_JOIN': true,
           'ENV': JSON.stringify(METADATA.ENV),
           'IDP_NAMESPACE': JSON.stringify('mcc-staging'),
           'HMR': METADATA.HMR,
